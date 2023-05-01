f63526dc589f4b137ece72dbe9056eae4050858c
Ukesh Kumar <ukeshsaravanan@gmail.com>
2023-05-01 12:03:28 +0530

    Initial Commit

---
+++ WeatherAppSwiftUI/Views/Main/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  WeatherAppSwiftUI
+//
+//  Created by UKESH KUMAR on 01/03/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HomeView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeatherAppSwiftUI/WeatherAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppSwiftUIApp.swift
+//  WeatherAppSwiftUI
+//
+//  Created by UKESH KUMAR on 01/03/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
